1f0f49441a46b27e83d0b55b187a5298994b74a0
Andy Mercer <andymercer@outlook.com>
2017-06-12 10:28:42 -0400

    Adding Initial Project Files
    When I first created this repo, I didn't set it up as a full project.

---
+++ gruntfile.js
@@ -0,0 +1,105 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		/***************************************/
+		/************ CONFIGURATION ************/
+		/***************************************/
+
+		config: {
+			build : 'build',
+			dist : 'dist'
+		},
+		path : '<%= config[process.argv[2]] %>', // process.argv[2] = current Grunt overall task. Like default, build, etc. Not sub tasks like uglify.
+
+		/***************************************/
+		/************* COMMON TASKS ************/
+		/***************************************/
+
+		/**** REMOVE OLD CONTENT ****/
+
+		clean: {
+			options: {
+				force: true
+			},
+			files: [
+				'<%= path %>/**/**/**'
+			]
+		},
+
+		/**** COPY ALL FILES ****/
+
+		copy: {
+			files: {
+				expand: true,
+				cwd: 'src',
+				src: [
+					'**/*',
+				],
+				dest: '<%= path %>' 
+			}
+		},
+
+		/**** PROCESS AND COPY CSS AND JS ****/
+
+		postcss: {
+			options: {
+				processors: [
+					require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes 
+					require('cssnano')({
+						reduceIdents: false,
+						discardUnused: false,
+						zindex: false
+					}) // minify the result
+				]
+			},
+			dist: {
+				files: [
+					{
+						expand: true,
+						cwd: '<%= path %>/css',
+						src: ['*.css', '!*.min.js'],
+						dest: '<%= path %>/css',
+					}
+				]
+			}
+		},
+		uglify: {
+			my_target: {
+				files: [
+					{
+						expand: true,
+						cwd: '<%= path %>/js',
+						src: ['*.js', '!*.min.js'],
+						dest: '<%= path %>/js',
+					}
+				]
+			}
+		},
+
+		/**** WATCH FOR CHANGES TO SOURCE FILES AND RERUN BUILD TASKS ****/
+
+		watch: {
+			scripts: {
+				files: ['src/**/**/*'],
+				tasks: ['clean', 'copy', 'postcss', 'uglify'],
+				options: {
+					spawn: false,
+				},
+			} 
+		},
+	});
+
+	// 2. Where we tell Grunt we plan to use this plug-in.
+	grunt.loadNpmTasks('grunt-postcss');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+
+	// 3. Where we tell Grunt what to do when we type "grunt" into the terminal.
+	grunt.registerTask('build', ['clean', 'copy', 'postcss', 'uglify', 'watch']);
+	grunt.registerTask('dist', ['clean', 'copy', 'postcss', 'uglify']);
+
+
+};
